5de1d6c5e9a60329e9c90700414961e02e9aead5
TheOliveOli <theoliveoli38@gmail.com>
2025-03-15 14:51:26 +0100

    Adjust sidebar & add status element

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,6 +1,8 @@
-module.exports = function(eleventyConfig) {
+module.exports = async function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets/*");
     eleventyConfig.addPassthroughCopy("assets/blog/*");
     eleventyConfig.addPassthroughCopy("style.css");
     eleventyConfig.addPassthroughCopy("style-blog.css");
+    const { EleventyRenderPlugin } = await import("@11ty/eleventy");
+	  eleventyConfig.addPlugin(EleventyRenderPlugin);
   };
