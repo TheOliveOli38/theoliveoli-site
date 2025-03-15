297bc10eac7a3acb7642ce9de9f940480258e2b9
TheOliveOli <theoliveoli38@gmail.com>
2025-03-15 22:24:51 +0100

    Add AeroRing files

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -3,6 +3,9 @@ module.exports = async function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets/blog/*");
     eleventyConfig.addPassthroughCopy("style.css");
     eleventyConfig.addPassthroughCopy("style-blog.css");
+    eleventyConfig.addPassthroughCopy("aeroring/pmoring.js");
+    eleventyConfig.addPassthroughCopy("aeroring/pmoring.config.js");
+    
     const { EleventyRenderPlugin } = await import("@11ty/eleventy");
 	  eleventyConfig.addPlugin(EleventyRenderPlugin);
   };
