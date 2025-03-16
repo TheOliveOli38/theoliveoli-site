cd9f4dab0a8c0580f2be8251ea3575ac204605af
TheOliveOli <theoliveoli38@gmail.com>
2025-03-16 15:43:45 +0100

    Add favicon and passthrough copy because i forgot

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,6 +1,7 @@
 module.exports = async function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets/*");
     eleventyConfig.addPassthroughCopy("assets/blog/*");
+    eleventyConfig.addPassthroughCopy("favicon.png");
     eleventyConfig.addPassthroughCopy("style.css");
     eleventyConfig.addPassthroughCopy("style-blog.css");
     eleventyConfig.addPassthroughCopy("aeroring/pmoring.js");
