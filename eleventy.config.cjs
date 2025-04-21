a4ca574d06cf0402d5779c2be7f2b0457a35ea04
TheOliveOli <theoliveoli38@gmail.com>
2025-04-21 14:04:45 +0200

    Readd apt ring assets

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,6 +1,7 @@
 module.exports = async function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets/*");
     eleventyConfig.addPassthroughCopy("assets/blog/*");
+    eleventyConfig.addPassthroughCopy("assets/webring/*");
     eleventyConfig.addPassthroughCopy("favicon.png");
     eleventyConfig.addPassthroughCopy("style.css");
     eleventyConfig.addPassthroughCopy("style-blog.css");
